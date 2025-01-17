507813f3647f780c1da372c73c3a55ccfa9eeab8
ItouRui <itourui.i@icloud.com>
2025-01-17 21:39:39 +0900

    feat(users): add documentation comments to service methods
    Added detailed documentation comments to the methods in the UsersService
    class.
    
    This includes descriptions of the methods, their parameters, and
    the return values.
    
    This improves code readability and maintainability.

--- apps/api/src/modules/users/users.service.ts
+++ apps/api/src/modules/users/users.service.ts
@@ -10,28 +10,35 @@ export class UsersService {
   ) {}
 
   /**
-   *
+   * Retrieves all users from the database.
+   * @returns A promise that resolves to an array of users.
    */
   async findAll() {
     return await this.userModel.find().exec();
   }
 
   /**
-   *
+   * Retrieves a user by their ID.
+   * @param id - The ID of the user to retrieve.
+   * @returns A promise that resolves to the user document, or null if not found.
    */
   async findOneById(id: string) {
     return await this.userModel.findById(new Types.ObjectId(id));
   }
 
   /**
-   *
+   * Retrieves a user by their Discord ID.
+   * @param discordId - The Discord ID of the user to retrieve.
+   * @returns A promise that resolves to the user document, or null if not found.
    */
   async findOneByDiscordId(discordId: string) {
     return await this.userModel.findOne({ discordId });
   }
 
   /**
-   *
+   * Creates a new user in the database.
+   * @param data - The data to create the user with.
+   * @returns A promise that resolves to the created user document.
    */
   async create(data: CreateUserDto) {
     return await this.userModel.create(data);
