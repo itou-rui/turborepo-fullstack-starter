0d203dd3e0c053f7b5c50fd1bd11de26f13d7e4e
Rui <itourui.i@icloud.com>
2025-02-12 11:18:00 +0900

    feat(web): add TurbelightNavBar to RootPageLayout
    - Imported TurbelightNavBar and icons from '@/components'
    - Updated RootPageLayout to include TurbelightNavBar
    - Added navigation items with icons for Home, About, Projects, and
    Resume
    - Applied styles for responsive design and layout adjustments

--- apps/web/src/app/(RootPage)/layout.tsx
+++ apps/web/src/app/(RootPage)/layout.tsx
@@ -1,5 +1,7 @@
-import { type LayoutProps } from '@/types';
 import { type Metadata } from 'next';
+import { type LayoutProps } from '@/types';
+import { TurbelightNavBar } from '@/components/Navbar';
+import { Home, User, Briefcase, FileText } from '@/components/Icons';
 
 (BigInt.prototype as any).toJSON = function () {
   return this.toString();
@@ -11,5 +13,20 @@ export const metadata: Metadata = {
 };
 
 export default async function RootPageLayout(props: LayoutProps) {
-  return <>{props.children}</>;
+  return (
+    <div className='relative min-h-screen bg-gradient-to-b from-zinc-900 to-black'>
+      <TurbelightNavBar
+        className='fixed'
+        items={[
+          { name: 'Home', url: '#', icon: Home },
+          { name: 'About', url: '#', icon: User },
+          { name: 'Projects', url: '#', icon: Briefcase },
+          { name: 'Resume', url: '#', icon: FileText },
+        ]}
+      />
+      <div className='flex flex-col min-h-screen'>
+        <main className='flex-1 container mx-auto md:px-4 sm:pt-24 pb-32'>{props.children}</main>
+      </div>
+    </div>
+  );
 }
