85f41e0017268668e1d6582085c58b3348dc3cf5
Rui <itourui.i@icloud.com>
2025-02-13 22:26:19 +0900

    refactor: no inline css support by default
    - Removed CrittersPlugin and related code from next.config.ts
    - Updated package.json to remove @workspace/critters dependency
    - Deleted postcss.config.mjs file
    - Simplified layout.tsx by removing unused imports and functions
    - Updated middleware.ts to remove addCustomHeader middleware
    - Deleted addCustomHeader.ts and its export from index.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,54 +1,9 @@
-import { writeFileSync } from 'fs';
-import { join } from 'path';
 import type { NextConfig } from 'next';
 import initializeBundleAnalyzer from '@next/bundle-analyzer';
-import CopyWebpackPlugin from 'copy-webpack-plugin';
-import { processHTMLFile, getFiles } from '@workspace/critters';
 
 const withBundleAnalyzer = initializeBundleAnalyzer({
   enabled: process.env.BUNDLE_ANALYZER_ENABLED === 'true',
 });
-
-class CrittersPlugin {
-  apply(compiler) {
-    compiler.hooks.done.tapAsync('CrittersPlugin', async (stats, callback) => {
-      if (process.env.NODE_ENV === 'production') {
-        console.time('Critters: build job');
-
-        const currentFolder = join(process.cwd(), '.next');
-        const files = getFiles(currentFolder);
-        const processedRoutes = [];
-
-        for (const file of files) {
-          if (file.endsWith('.html')) {
-            const pagesFolder = file.split('.next/server/pages')[1];
-            if (pagesFolder) {
-              await processHTMLFile(file, pagesFolder).catch((e) => {
-                console.error('[Warning][PageFolder]: ' + e.message);
-              });
-
-              processedRoutes.push(pagesFolder.replace('.html', '').replace('index', ''));
-            }
-
-            const appFolder = file.split('.next/server/app')[1];
-            if (appFolder) {
-              await processHTMLFile(file, appFolder).catch((e) => {
-                console.error('[Warning][AppFolder]: ' + e.message);
-              });
-
-              processedRoutes.push(appFolder.replace('.html', '').replace('index', ''));
-            }
-          }
-        }
-
-        writeFileSync(join(process.cwd(), 'processedRoutes.json'), JSON.stringify(processedRoutes));
-
-        console.timeEnd('Critters: build job');
-      }
-      callback();
-    });
-  }
-}
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
@@ -69,22 +24,6 @@ const nextConfig: NextConfig = {
         hostname: 'ui.shadcn.com',
       },
     ],
-  },
-
-  webpack: (config, { isServer }) => {
-    config.plugins.push(
-      new CopyWebpackPlugin({
-        patterns: [{ from: 'styles', to: 'static/css' }],
-      }),
-    );
-
-    config.plugins.push(new CrittersPlugin());
-
-    return config;
-  },
-
-  experimental: {
-    optimizeCss: true,
   },
 };
 
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,7 +1,4 @@
-import { join } from 'path';
-import fs from 'fs';
 import { Geist, Geist_Mono } from 'next/font/google';
-import { headers } from 'next/headers';
 import { type JSX } from 'react';
 import { Toaster } from '@workspace/ui/components/sonner';
 import { type LayoutProps } from '@/types';
@@ -19,59 +16,17 @@ const fontMono = Geist_Mono({
 });
 
 /**
- * Get the paths to the CSS files based on the page.
- * @param {string | null} page - The current page path.
- * @returns {string[]} - An array of CSS file paths.
- */
-function getCSSPaths(page: string | null): string[] {
-  const basePath = join('/_next', 'static', 'css');
-  const globalCSS = join(basePath, 'globals.css');
-  if (page?.startsWith('/dynamic')) {
-    return [join(basePath, 'dynamic.css'), globalCSS];
-  }
-  return [globalCSS];
-}
-
-/**
- * Get the critical CSS for the given page.
- * @param {string | null} page - The current page path.
- * @returns {Promise<JSX.Element | false>} - A promise that resolves to a style element with the critical CSS or false if not found.
- */
-async function getCriticalCSS(page: string | null): Promise<JSX.Element | false> {
-  if (!page) return false;
-
-  const withoutQuery = page.split('?')[0];
-  const cssPath = join(process.cwd(), 'critters', withoutQuery, 'styles.css');
-
-  try {
-    const cssContent = await fs.promises.readFile(cssPath, 'utf-8');
-    return <style dangerouslySetInnerHTML={{ __html: cssContent }} />;
-  } catch {
-    return false;
-  }
-}
-
-/**
  * The root layout component for the application.
  * @param {LayoutProps} props - The layout properties.
  * @returns {JSX.Element} - The root layout component.
  */
 export default async function RootLayout(props: LayoutProps): Promise<JSX.Element> {
-  const headersList = headers();
-  const pathName = (await headersList).get('x-pathname');
-  const criticalCSS = await getCriticalCSS(pathName);
-  const isCriticalCSSMode = process.env.CRITTERS_RUNTIME && criticalCSS;
-
-  const cssLinks = getCSSPaths(pathName).map((link) => <link key={link} rel='stylesheet' href={link} />);
-
   return (
     <html lang='en' suppressHydrationWarning>
-      <head>{isCriticalCSSMode ? criticalCSS : cssLinks}</head>
       <body className={`${fontSans.variable} ${fontMono.variable} font-sans`}>
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
           <ReduxProvider>
             {props.children}
-            {isCriticalCSSMode && cssLinks}
             <Toaster />
           </ReduxProvider>
         </ThemeProvider>
