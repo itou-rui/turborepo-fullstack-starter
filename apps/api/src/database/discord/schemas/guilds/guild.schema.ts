6e3b793d23b93460b185b2c03386ef1d84c70b07
Rui <itourui.i@icloud.com>
2025-03-01 20:00:02 +0900

    feat(api): add Discord database integration
    - Integrated Mongoose for Discord database connection in app.module.ts
    - Updated database.config.ts to include Discord database configuration
    - Added schemas for Command and Guild in Discord database
    - Created types for ICommand and IGuild in packages/types
    - Updated command.service.ts to log updated commands

---
+++ apps/api/src/database/discord/schemas/guilds/guild.schema.ts
@@ -0,0 +1,21 @@
+import { type Model, type HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { type OmmitedBaseModelFields, type IGuild } from '@workspace/types';
+import { BaseDocument, BaseDocumentSchema } from '../../../common';
+
+@Schema()
+export class Guild extends BaseDocument implements Omit<IGuild, OmmitedBaseModelFields> {
+  @Prop({ required: true, unique: true })
+  uid!: string;
+
+  @Prop({ required: true })
+  name!: string;
+}
+
+export type GuildDocument = HydratedDocument<Guild>;
+
+export type GuildModel = Model<GuildDocument>;
+
+export const GuildSchema = SchemaFactory.createForClass(Guild);
+
+GuildSchema.add(BaseDocumentSchema);
--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -2,3 +2,5 @@ export * from './common';
 export * from './users';
 export * from './sessions';
 export * from './auth';
+export * from './guilds';
+export * from './commands';
