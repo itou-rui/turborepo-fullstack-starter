a52f35c86ce93b3bdcac1387667b0305cd8e832a
Rui <itourui.i@icloud.com>
2025-03-01 20:53:11 +0900

    refactor(users): modularize services and controllers
    - Import all services and controllers using wildcard imports
    - Flatten the imported services and controllers into arrays
    - Update `providers`, `controllers`, and `exports` to use the flattened
    arrays

--- apps/api/src/modules/users/users.module.ts
+++ apps/api/src/modules/users/users.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '../../database/main';
-import { UsersService } from './services';
-import { UsersController } from './controllers';
+import * as Services from './services';
+import * as Controllers from './controllers';
+
+const services = Object.values(Services).flat();
+const controllers = Object.values(Controllers).flat();
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }], 'main')],
-  providers: [UsersService],
-  controllers: [UsersController],
-  exports: [UsersService],
+  providers: [...services],
+  controllers: [...controllers],
+  exports: [...services],
 })
 export class UsersModule {}
--- packages/types/src/users/index.ts
+++ packages/types/src/users/index.ts
@@ -1,3 +1,3 @@
-export * from "./models";
-export * from "./requests";
-export * from "./responses";
+export * from './models';
+export * from './requests';
+export * from './responses';
