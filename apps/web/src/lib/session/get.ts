80d935967dff92fbe001f265d91d42c69699f838
Rui <itourui.i@icloud.com>
2025-03-03 18:49:50 +0900

    refactor(session): simplify session fetch logic
    - Removed logging from get function in session fetch logic.
    - Added headers and credentials to fetcher.get call.
    - Simplified error handling by removing try-catch block.
    - Updated function to use headers from 'next/headers'.

--- apps/web/src/lib/session/get.ts
+++ apps/web/src/lib/session/get.ts
@@ -1,42 +1,20 @@
 'use server';
 
+import { headers } from 'next/headers';
 import { type APISession } from '@workspace/types';
-import { NextStructuredLogger, type LogFormat } from '@workspace/logger';
 import * as fetcher from '../fetcher';
 
-export async function get(revalidate = 30): Promise<APISession | null> {
-  const logger = new NextStructuredLogger({
-    name: 'GetSession',
-    format: process.env.LOG_FORMAT as LogFormat,
+export async function get(): Promise<APISession | null> {
+  const headerList = await headers();
+
+  const result = await fetcher.get<APISession>('/api/auth/session', {
+    headers: new Headers(headerList),
+    credentials: 'include',
   });
 
-  try {
-    const result = await fetcher.get<APISession>('/api/auth/session', {
-      revalidate,
-    });
-
-    // 2xx
-    if (result.ok === true) {
-      logger.info(`GET /api/auth/session ${result.status} ${result.message}`);
-      return result.data;
-    }
-
-    // 4xx, 5xx
-    else {
-      logger.error(`GET /api/auth/session ${result.status} ${result.message}`, {
-        code: result.code,
-        errors: result.errors,
-      });
-      return null;
-    }
-  } catch (e: unknown) {
-    const entry = { error: e, sourceLocation: { file: 'index.ts', line: '11-14', function: 'getSession' } };
-
-    if (e instanceof Error) {
-      logger.error(`GET /api/auth/session ${e.message}`, entry);
-    } else {
-      logger.error(`GET /api/auth/session exception error`, entry);
-    }
-    return null;
+  if (result.ok) {
+    return result.data;
   }
+
+  return null;
 }
