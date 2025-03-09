3868839e67ddfa3ebc342c4db4037f61ce5d98a5
Rui <itourui.i@icloud.com>
2025-03-09 23:26:10 +0900

    refactor(api): update guild model types
    - Replace IGuild with IGuildModel in guilds.service.ts
    - Create new APICommand and ICommand types
    - Move and rename ICommand and IGuild files
    - Update index exports to reflect new structure

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -2,5 +2,5 @@ export * from './Auth';
 export * from './Result';
 export * from './Base';
 export * from './User';
-export * from './IGuild';
-export * from './ICommand';
+export * from './Guild';
+export * from './Command';
