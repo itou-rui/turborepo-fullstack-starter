95f80440d2128f1faf3dff4428959d5cdd803423
Rui <itourui.i@icloud.com>
2025-03-09 21:21:59 +0900

    feat(api): add Discord authentication module
    Integrated Discord authentication into the API.
    
    Added new controllers, services, strategies, and repositories for
    handling Discord OAuth.
    
    Updated the AuthModule to include Discord authentication components.

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -2,11 +2,19 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users';
 import { LocalAuthService, LocalAuthController, LocalStrategy, LocalAuthSessionSerializer } from './local';
+import { DiscordAuthRepository, DiscordAuthController, DiscordAuthService, DiscordStrategy } from './discord';
 
 @Module({
   imports: [UsersModule, PassportModule],
-  controllers: [LocalAuthController],
-  providers: [LocalAuthService, LocalStrategy, LocalAuthSessionSerializer],
-  exports: [LocalAuthService],
+  controllers: [LocalAuthController, DiscordAuthController],
+  providers: [
+    LocalAuthService,
+    LocalStrategy,
+    LocalAuthSessionSerializer,
+    DiscordAuthService,
+    DiscordStrategy,
+    DiscordAuthRepository,
+  ],
+  exports: [LocalAuthService, DiscordAuthService],
 })
 export class AuthModule {}
