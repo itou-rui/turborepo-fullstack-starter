5e48f6fd0ecde566591b58b6ef168798572bbf20
Rui <itourui.i@icloud.com>
2025-03-18 16:06:56 +0900

    feat(constants): add detailed error codes
    Added detailed error codes for general, account/user, registration,
    and authentication related errors in RESTAPIErrorJSONCodes enum.

--- packages/constants/src/errors/code.ts
+++ packages/constants/src/errors/code.ts
@@ -1,11 +1,30 @@
 export enum RESTAPIErrorJSONCodes {
-  GeneralError,
+  // 1000-1999: General Errors
+  GeneralError = 1000,
   BadRequest,
+  Unauthorized,
+  Forbidden,
+  NotFound,
+  MethodNotAllowed,
+  Conflict,
+  TooManyRequests,
+  InternalServerError,
+  ServiceUnavailable,
 
+  // 10000-10999: Account/User Related
   UnknownAccount = 10_001,
   UnknownUser,
+  AccountDisabled,
+  AccountLocked,
+  EmailNotVerified,
 
+  // 20000-20999: Registration Related
   AlreadyUser = 20_001,
+  InvalidUsername,
+  WeakPassword,
+  DuplicateEmail,
 
+  // 30000-30999: Authentication Related
   InvalidCredentials = 30_001,
+  SessionExpired,
 }
