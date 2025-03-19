95f34468d55b02d2c1d8f146fbd984a64450c0e8
Rui <itourui.i@icloud.com>
2025-03-19 18:59:40 +0900

    feat(api): add user not found exception handling
    - Added UserNotFoundException to handle cases where a user is not found.
    - Updated UsersController to throw UserNotFoundException when a user is
    not found.

--- apps/api/src/modules/users/users.controller.ts
+++ apps/api/src/modules/users/users.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Param, UseFilters } from '@nestjs/common';
 import { type APIUser } from '@workspace/types';
 import { HttpExceptionFilter } from 'utils/filters';
 import { UsersService } from './users.service';
+import { UserNotFoundException } from 'utils/exceptions';
 
 @UseFilters(HttpExceptionFilter)
 @Controller('users')
@@ -17,6 +18,7 @@ export class UsersController {
   @Get(':uid')
   async findOne(@Param('uid') uid: string): Promise<APIUser | null> {
     const user = await this.usersService.findOneByUid(uid);
-    return user ? this.usersService.toAPIUser(user) : null;
+    if (!user) throw new UserNotFoundException(uid);
+    return this.usersService.toAPIUser(user);
   }
 }
