8b549ce09db37be1131fce5c4d8819a436880c34
Rui <itourui.i@icloud.com>
2025-03-19 13:09:14 +0900

    fix(api): correct typo in exception export
    - Fix typo in resource.exceptions export
    - Add UserNotFoundException for handling user not found errors

--- apps/api/src/utils/exceptions/index.ts
+++ apps/api/src/utils/exceptions/index.ts
@@ -2,3 +2,4 @@ export * from './api.exception';
 export * from './conflict.exceptions';
 export * from './bad-request.exceptions';
 export * from './internal-server-error.exceptions';
+export * from './not-found.exceptions';
---
+++ apps/api/src/utils/exceptions/not-found.exceptions.ts
@@ -0,0 +1,9 @@
+import { type RESTErrorData } from '@workspace/types';
+import { RESTAPIErrorJSONCodes } from '@workspace/constants';
+import { NotFoundAPIException } from './http-exceptions';
+
+export class UserNotFoundException extends NotFoundAPIException {
+  constructor(uid: string, errors?: RESTErrorData) {
+    super(RESTAPIErrorJSONCodes.NotFound, `User with ID ${uid} not found.`, errors);
+  }
+}
